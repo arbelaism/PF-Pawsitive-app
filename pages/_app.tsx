49c13161b2a30a56a90f592d1f2d75d5c43da563
arbelais <arbelaism@outlook.com>
2022-11-22 20:56:01 -0300

    deleted vercel svg, home.module.css. Updated _app.tsx and index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Pawsitive App</title>
+                <meta
+                    name="description"
+                    content="App created for Henry focused on adoptions and many other services for your pets"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
