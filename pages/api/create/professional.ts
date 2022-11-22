53a6d6766698bb3f9f867229ad6d53d0dd78c64f
Zadckiel <cris-a2112@hotmail.com>
2022-11-22 17:57:24 -0500

    create professional get professionals

---
+++ pages/api/create/professional.ts
@@ -0,0 +1,27 @@
+import { prisma } from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { businessName, contact, address, description, photo, dni, type, qualification, ownerBusinessId  } = req.body;
+    try {
+        await prisma.professionalBusiness.create({
+            data: {
+                businessName,
+                contact,
+                address,
+                description,
+                photo,
+                dni,
+                type,
+                qualification,
+                ownerBusinessId,   //ownerBusinessId : user.id 
+            }
+        })
+
+        res.status(200).json({ message: "created" })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ message: "error" })
+    }
+
+}
