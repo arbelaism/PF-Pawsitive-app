29eda883a8c04d50f98a55a7bddaba27f3e1e197
davGuzMal <dgm0519@hotmail.com>
2022-11-23 09:36:58 +0100

    create and get items

---
+++ pages/api/read/Items.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+
+export default async function getItems(req: NextApiRequest, res: NextApiResponse) {
+    //const { name, price, description, amount, photo, active, usuario } = req.body;
+    try {       
+
+        const Items = await prisma.items.findMany({
+            where:{
+                active:true
+            },
+            select:{
+                name: true,
+                price: true,
+                amount: true,
+                profesionalBusiness: {
+                    select: {
+                        businessName:true
+                    }
+                }
+            }
+        })
+        
+        
+        res.status(200).json(Items)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ message: "error" })
+    }
+
+}
