2582e2aef0db06a2c159cb30e69858d0b113d502
Nestor <ndqa96@gmail.com>
2022-11-23 14:32:38 -0400

    Auth0 Provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import {Auth0Provider} from '@auth0/auth0-react'; 
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<Auth0Provider 
+            domain='dev-da1yc0rjs8j70lg0.us.auth0.com' 
+            clientId='KgyRDNtn6UdwHK7ty1cYLqV4qTMb4sf8' 
+            redirectUri={window.location.origin}
+            >
+              <Component {...pageProps} />
+          </Auth0Provider>
+          )
 }
 
 export default MyApp
