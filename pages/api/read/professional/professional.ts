084e826e7003f4c6f9aa422fe0567e936431fd5f
Cristian Acevedo Posada <cris-a2112@hotmail.com>
2022-11-23 12:49:57 -0500

    dynamic api routes professional id and type

---
+++ pages/api/read/professional/professional.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const professionals = await prisma.professionalBusiness.findMany()
+        if(professionals.length > 0) {
+            res.status(200).json(professionals)
+        }else{
+            throw new Error
+        }     
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ message: "error " })
+    }
+
+} 
