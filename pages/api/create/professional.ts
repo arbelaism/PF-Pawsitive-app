e3c8198689b185eaab7208c74dfecde8d8ee1a7e
Cristian Acevedo Posada <cris-a2112@hotmail.com>
2022-11-23 09:22:03 -0500

    created get professional schema change

--- pages/api/create/professional.ts
+++ pages/api/create/professional.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 dni,
                 type,
                 qualification,
-                ownerBusinessId,   //ownerBusinessId : user.id 
+                ownerBusinessId,  
             }
         })
 
---
+++ pages/api/create/user.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { name, lastName, email, age, role, password } = req.body;
+    try {
+        await prisma.user.create({
+            data: {
+                name,
+                lastName,
+                email,
+                age,
+                role,
+                password
+            }
+        })
+        res.status(200).json({ message: "created" })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ message: "error" })
+    }
+
+}
+
