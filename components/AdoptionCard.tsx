77a9dbde9fdd6e63db09c21361d2c2548e48b61e
arbelais <arbelaism@outlook.com>
2022-11-24 13:48:05 -0300

    adoption-card

---
+++ components/AdoptionCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdoptionCard = () => {
+  return (
+    <div>AdoptionCard</div>
+  )
+}
+
+export default AdoptionCard
--- components/index.ts
+++ components/index.ts
@@ -3,4 +3,5 @@ export { default as Navbar } from './Navbar'
 export { default as Footer } from './Footer'
 export { default as Login } from './Login'
 export { default as SignUp } from './SignUp'
+export { default as AdoptionCard } from './AdoptionCard'
 
