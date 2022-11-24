c09852a490e3740d885dc0b604aae933dc2505b8
arbelais <arbelaism@outlook.com>
2022-11-24 20:32:01 -0300

    Adoptions cards. Modified globals.css to light theme.

--- components/AdoptionCard.tsx
+++ components/AdoptionCard.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Image from 'next/image'
+import styles from '../styles/AdoptionCard.module.css'
 
-const AdoptionCard = () => {
-  return (
-    <div>AdoptionCard</div>
-  )
+type Adoption = {
+    name: string
+    size: string
+    age: string
+    breed: string
+    photo: string
+    active: boolean
+}
+
+const AdoptionCard = ({ name, size, age, breed, photo, active }: Adoption) => {
+    return (
+        <div className={styles.cardContainer}>
+            {active ? (
+                <div className={styles.card}>
+                    <Image
+                        src={photo}
+                        alt="No image to display"
+                        width={150}
+                        height={150}
+                        className={styles.cardImage}
+                    />
+                    <h2 className={styles.title}>{name}</h2>
+                    <p className={styles.text}>Size: {size}</p>
+                    <p className={styles.text}>Age: {age}</p>
+                    <p className={styles.text}>Breed: {breed}</p>
+                </div>
+            ) : null}
+        </div>
+    )
 }
 
 export default AdoptionCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
 import type { NextPage } from 'next'
-import { MainLayout } from '../components'
+import { AdoptionCard, MainLayout } from '../components'
 
 const Home: NextPage = () => {
     return (
         <MainLayout title="Pawsitive - Home">
-            <div>
-                <h1>Home component</h1>
-            </div>
+            {/* <div className="flex flex-wrap"> */}
+            {/*     {adoptions */}
+            {/*         ? adoptions.map(adoption => { */}
+            {/*               return adoption.active ? ( */}
+            {/*                   <AdoptionCard */}
+            {/*                       key={adoption.id} */}
+            {/*                       name={adoption.name} */}
+            {/*                       size={adoption.size} */}
+            {/*                       age={adoption.age} */}
+            {/*                       breed={adoption.breed} */}
+            {/*                       photo={adoption.photo} */}
+            {/*                       active={adoption.active} */}
+            {/*                   /> */}
+            {/*               ) : null */}
+            {/*           }) */}
+            {/*         : null} */}
+            {/* </div> */}
         </MainLayout>
     )
 }
