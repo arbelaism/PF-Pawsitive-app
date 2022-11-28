7d9d8db307ddefc230b2df0038bfb338cd611f35
Cristian Acevedo Posada <cris-a2112@hotmail.com>
2022-11-27 23:35:53 -0500

    Functional contact form
    formulario de contacto

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,34 +1,77 @@
 import { NextComponentType } from "next";
-import { Footer, Navbar } from "../components";
+import { MainLayout } from "../components";
 import styles from "../styles/Contact.module.css";
+import { useForm, SubmitHandler } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
-const contact: NextComponentType = () => {
+interface ContactForm {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const Contact: NextComponentType = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ContactForm>();
+
+  const onSubmit: SubmitHandler<ContactForm> = async (data) => {
+    try {
+      emailjs
+        .send(
+          "service_crfov1j",
+          "template_ykzfued",
+          {
+            from_name: "Pawsitive team",
+            to_name: `${data.name}`,
+            message: `contest this mail to start a conversation with us`,
+            reply_to: `${data.email}`,
+          },
+          "Vu6_KG-xIf-x55P1I"
+        )
+        .then(
+          function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          function (error) {
+            console.log("FAILED...", error);
+          }
+        );
+    } catch (error) {
+      console.log(error + "Incorrect data form");
+    }
+  };
+
   return (
-    <>
-      <Navbar />
+    <MainLayout title="Contact">
       <div className={styles.container}>
-        <form className={styles.formS}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.formS}>
           <h2 className={styles.h2S}>CONTACT US</h2>
           <p className={styles.pS}>
             <label className={styles.labelS}> Name </label>
             <input
               className={styles.inputS}
               placeholder="Write your name here.."
-            ></input>
+              {...register("name", { required: true, maxLength: 20 })}
+            />
           </p>
           <p className={styles.pS}>
             <label className={styles.labelS}> Email </label>
             <input
               className={styles.inputS}
               placeholder="Let us know how to contact you back.."
-            ></input>
+              {...register("email", { required: true, maxLength: 40 })}
+            />
           </p>
           <p className={styles.pS}>
             <label className={styles.labelS}> Message </label>
             <input
               className={styles.inputS}
               placeholder="What would you like to tell us.."
-            ></input>
+              {...register("message", { required: true, maxLength: 20 })}
+            />
           </p>
           <button className={styles.buttonS}>Send Message</button>
           <div className={styles.divS}>
@@ -37,8 +80,7 @@ const contact: NextComponentType = () => {
           </div>
         </form>
       </div>
-      <Footer />
-    </>
+    </MainLayout>
   );
 };
-export default contact;
+export default Contact;
