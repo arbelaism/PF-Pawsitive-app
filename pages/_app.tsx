0ee96f36658bacaeaa40b568a60e15b4e381525e
Nestor <ndqa96@gmail.com>
2022-11-29 14:14:35 -0400

    React-query provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'styles/globals.css';
-import {QueryClientProvider, QueryClient} from 'react-query';
+import {QueryClientProvider, QueryClient} from 'react-query'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
