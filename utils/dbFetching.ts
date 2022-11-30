220081659eb3220c5d62448ce634ec35aa462d74
Nestor <ndqa96@gmail.com>
2022-11-29 23:57:50 -0400

    Get post by ID added

--- utils/dbFetching.ts
+++ utils/dbFetching.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import {useQuery} from 'react-query';
+import { Props } from "pages/adoptions";
 
 export const getAdoptions= async () => {
     const response = await axios.get('http://localhost:3000/api/adoptionpost');
@@ -8,4 +10,12 @@ export const getAdoptions= async () => {
         throw new Error('Data not found')
     }
     return adoptions;
-}
+};
+//Custom hook to fetch data by id
+const fetchPet = (id: any)=>{
+    return axios.get(`http://localhost:3000/api/adoptionpost/${id}`);
+};
+//Data query by id
+export const getPetById = (id: any)=>{
+    return useQuery(['pet', id], ()=> fetchPet(id))
+};
