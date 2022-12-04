acfce231edf6059d9ad8fbc982ac58b45de719c7
arbelais <arbelaism@outlook.com>
2022-12-04 20:43:05 -0300

    nada

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 import 'styles/globals.css'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
     const queryClient = new QueryClient()
