d74b4aa4ffc7338c799fd00e6c91e7e38f2fff08
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2022-12-05 11:58:11 -0300

    fixed number displayed navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,82 +7,86 @@ import { Product } from "@prisma/client";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Navbar: NextComponentType = () => {
-  const [cartProducts, setCartProducts] = useState([0]);
+    const [cartProducts, setCartProducts] = useState([0]);
     const [products, setProducts] = useLocalStorage<Product[]>("cartProducts", [])
+    const [numberDisplayed, setNumberDisplayed] = useState(0)
+    const { user, error, isLoading } = useUser()
 
-    const {user, error, isLoading} = useUser()
-
-  useEffect(() => {
-    // const saved = localStorage.getItem("cartProducts" || "[]");
-    // const products = JSON.parse(saved!);
+    useEffect(() => {
+        // const saved = localStorage.getItem("cartProducts" || "[]");
+        // const products = JSON.parse(saved!);
+        if (cartProducts.length > 0) {
+            let numb = cartProducts.reduce((a, b) => a + b, 0)
+            setNumberDisplayed(numb)
+        }
         if (products.length > 0)
             setCartProducts(products.map((p: any) => p.amount))
-  }, [products]);
-  return (
-      <>
-          <nav className="w-full bg-yellow-300">
-              <div className={styles.navContainer}>
-                  <div className={styles.logoContainer}>
-                      <Link href={'/'}>
-                          <a>
-                              <span className={styles.paw}>Paw</span>
-                              <span className={styles.sitive}>sitive</span>
-                          </a>
-                      </Link>
-                  </div>
-                  <div className={styles.linksContainer}>
-                      <div>
-                          <Link href={'/'}>
-                              <a>Home</a>
-                          </Link>
-                      </div>
-                      <div>
-                          <Link href={'/adoptions'}>
-                              <a>Adopciones</a>
-                          </Link>
-                      </div>
-                      <div>
-                          <Link href={'/products'}>
-                              <a>Productos</a>
-                          </Link>
-                      </div>
-                      <div>
-                          <Link href={'/contact'}>
-                              <a>Contacto</a>
-                          </Link>
-                      </div>
-                      <div>
-                          <Link href={'/about'}>
-                              <a>About</a>
-                          </Link>
-                      </div>
-                  </div>
-                  <div className={styles.signUpContainer}>
-                      <div>
-                          <Link href={'/shoppingCart'}>
-                              <a>
-                                  🛒 {cartProducts.reduce((a, b) => a + b, 0)}
-                              </a>
-                          </Link>
-                      </div>
-                      {!user ? (
-                          <div className={styles.signUpLink}>
-                              <Link href="/api/auth/login">
-                                  <a>Login</a>
-                              </Link>
-                          </div>
-                      ) : (
-                          <>
-                              <span>Hola {user.name} &nbsp;&nbsp;| </span>
-                              <Link href="/api/auth/logout">
-                                  <a>Logout</a>
-                              </Link>
-                          </>
-                      )}
-                  </div>
-              </div>
-          </nav>
-      </>
-  )
+    }, [cartProducts, products]);
+    return (
+        <>
+            <nav className="w-full bg-yellow-300">
+                <div className={styles.navContainer}>
+                    <div className={styles.logoContainer}>
+                        <Link href={'/'}>
+                            <a>
+                                <span className={styles.paw}>Paw</span>
+                                <span className={styles.sitive}>sitive</span>
+                            </a>
+                        </Link>
+                    </div>
+                    <div className={styles.linksContainer}>
+                        <div>
+                            <Link href={'/'}>
+                                <a>Home</a>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link href={'/adoptions'}>
+                                <a>Adopciones</a>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link href={'/products'}>
+                                <a>Productos</a>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link href={'/contact'}>
+                                <a>Contacto</a>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link href={'/about'}>
+                                <a>About</a>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className={styles.signUpContainer}>
+                        <div>
+                            <Link href={'/shoppingCart'}>
+                                <a>
+                                    🛒 {numberDisplayed}
+                                </a>
+                            </Link>
+                        </div>
+                        {!user ? (
+                            <div className={styles.signUpLink}>
+                                <Link href="/api/auth/login">
+                                    <a>Login</a>
+                                </Link>
+                            </div>
+                        ) : (
+                            <>
+                                <span>Hola {user.name} &nbsp;&nbsp;| </span>
+                                <Link href="/api/auth/logout">
+                                    <a>Logout</a>
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 };
 export default Navbar;
