def9bf74fdd6c29706eab7c279f76206e11ae474
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2022-12-06 00:19:22 -0300

    api for balance and get payments

---
+++ pages/api/product/get_payment/index.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function payment(req: NextApiRequest, res: NextApiResponse) {
+    const Stripe = require('stripe');
+    const stripe = new Stripe("sk_test_51M9y5pEAdwcBn8LBUJhWL1X4Qgj4vN1PpZltKGdMg6dgCbouXSuRiYefasaaysNik3NdoMX4HdnpDEZNrDkQ3eiW00A289ySnS");
+    
+    
+    try {
+        const paymentIntents = await stripe.paymentIntents.list();
+        // console.log(payment);
+        res.status(200).json(paymentIntents);
+
+    } catch (error: any) {
+        console.log(error.raw.message)
+        res.status(401).send({ Error: error.raw.message });
+    }
+
+};
