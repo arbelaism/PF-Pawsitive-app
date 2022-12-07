b119a28c824fe411ace7e27aa58096821ac8a5e0
Nestor <ndqa96@gmail.com>
2022-12-06 20:51:25 -0400

    Product card styles modified

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
-import { Product, UserProduct } from 'app/types';
-import styles from 'styles/ProductCard.module.css'
+import { Product } from 'app/types';
 import Link from 'next/link'
-import { style } from '@mui/system';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
 
 type Props = {
     id: string
@@ -12,34 +11,72 @@ type Props = {
 }
 
 const ProductCard = ({ id, product, handleAddToCart }: Props) => {
+
+    const productR: Number = Math.floor(4.5);
+    let stars= [];
+    for(let i = 0; i<5; i++){
+        if(i<productR) stars.push(true);
+        if(i>=productR) stars.push(false);
+    }
+
     return (
-        <div className={styles.cardContainer} key={id}>
-            <div className={styles.card} key={id}>
-                <Image
-                    src={product.photo}
-                    alt="No image to display"
-                    width={150}
-                    height={150}
-                    className={styles.cardImage}
-                />
-                <div className="flex flex-col gap-2 justify-between items-center">
-                    <h2 className={styles.title}>{product.name}</h2>
-                    <p className={styles.description}>{product.description}</p>
-                    <div className='flex items-center gap-3'>
-                        <p className={styles.text}>Tamaño: {product.size}</p>
-                        <p className={styles.text}>
-                            Precio: ${product.displayPrice}
-                        </p>
+        
+<div key={id} >
+    <div className="flex flex-col justify-between items-center m-5 bg-white shadow-md rounded-lg w-60 h-80 shadow-xl">
+        <div className='h-1/8'>
+            <Image className="rounded-t-lg p-8" src={product.photo} alt="product image" width={120} height={120}/>
+        </div>          
+        <div className="flex flex-col justify-between items-center h-7/8 z-1000">
+            <div className='px-5'>
+                <h3 className="text-gray-900 font-Rubik">
+                    {product.name}
+                </h3>
+            </div>
+            
+            <div className="flex items-center mb-2.5">
+                {stars.map(star=>{
+                    if(star===true){
+                        return <AiFillStar/>
+                    } 
+                    if(star===false){
+                        return <AiOutlineStar/>
+                    } 
+                })}
+            </div>
+            <div className='flex flex-row justify-center items-center mb-1 mt-1 font-Rubik'>
+                <div className='px-2.5'>
+                    <span className="text-lg text-pwpurple-700">
+                        ${product.displayPrice}
+                    </span>
+                </div>
+                <div className='px-2.5'>
+                    <div>
+                        <button onClick={() => 
+                            handleAddToCart(product)} 
+                            disabled = {product.stock<=1 && true} 
+                            className="text-l font-bold rounded-lg px-1 py-1 border-2 border-pwpurple-700 text-pwpurple-700 hover:bg-pwpurple-700 hover:text-pwpurple-100 duration-300"
+                            >
+                            +Carrito
+                        </button>
                     </div>
                 </div>
-                <button
-                    disabled = {product.stock<=1 && true}
-                    className={styles.button}
-                    onClick={() => handleAddToCart(product)}>
-                    Agregar al carrito
-                </button>
+
+            </div>
+            <div className='pb-5'>
+                <Link href="/products/:id">
+                    <a className="text-pwgreen-500  text-md font-semibold">
+                        Ver detalles
+                    </a>
+                </Link>
             </div>
         </div>
+    </div>
+
+</div>
+
+
+            
+        
     )
 }
 
