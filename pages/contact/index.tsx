bf2b1d727484472e4e7aa11e1a96c604cd16e0ab
Cristian Acevedo Posada <cris-a2112@hotmail.com>
2022-12-07 20:31:16 -0500

    contactForm tailwind
    contactForm tailwind

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,10 +1,9 @@
 import { NextComponentType } from "next";
 import { MainLayout } from "components";
-import styles from "styles/Contact.module.css";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
-import { useRouter } from "next/router"
-import { sendMail } from "utils/dbFetching"
+import { useRouter } from "next/router";
+import { sendMail } from "utils/dbFetching";
 import { ContactForm } from "../../app/types";
 import { alerts } from "utils/alerts";
 
@@ -14,70 +13,100 @@ const Contact: NextComponentType = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<ContactForm>();
-  
-  const router = useRouter()
+
+  const router = useRouter();
   const queryClient = useQueryClient();
   const { mutate, isLoading } = useMutation(sendMail, {
-    onSuccess: data => {
+    onSuccess: (data) => {
       // console.log(data);
-      alerts({icon: 'info', title: '<strong>Email</strong>', text: 'Email Sent', toast: true})
-      router.push("/") 
+      alerts({
+        icon: "info",
+        title: "<strong>Email</strong>",
+        text: "Email Sent",
+        toast: true,
+      });
+      router.push("/");
     },
     onError: () => {
-      alerts({icon: 'error', title: '<strong>Email</strong>', text: 'Cant send the mail', toast: true})
+      alerts({
+        icon: "error",
+        title: "<strong>Email</strong>",
+        text: "Cant send the mail",
+        toast: true,
+      });
     },
     onSettled: () => {
-      queryClient.invalidateQueries('create');
-    }
+      queryClient.invalidateQueries("create");
+    },
   });
-  
+
   const onSubmit: SubmitHandler<ContactForm> = async (data) => {
     const mail = {
       ...data,
-      action : "contact",
+      action: "contact",
     };
     const email = {
       ...data,
-      action : "contactUs",
+      action: "contactUs",
     };
-    mutate(email)
-    mutate(mail)
-
+    mutate(email);
+    mutate(mail);
   };
-  
   return (
     <MainLayout title="Contact">
-      <div className={styles.container}>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.formS}>
-          <h2 className={styles.h2S}>CONTACT US</h2>
-          <p className={styles.pS}>
-            <label className={styles.labelS}> Name </label>
+      <div className="relative z-0 mb-6 w-full group mt-[15%] mb-[30%]">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="m-2.5 pb-2.5 text-pwgreen-600 indent-[50%] border-b-2 border-pwgreen-700 ">CONTACTANOS</h2>
+          <div className="relative z-0 mb-6 w-full group">
             <input
-              className={styles.inputS}
-              placeholder="Write your name here.."
+              type="text"
+              id=""
+              className="block py-2.5 px-0 w-full text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-pwgreen-600 peer"
+              placeholder=" "
               {...register("name", { required: true, maxLength: 20 })}
             />
-          </p>
-          <p className={styles.pS}>
-            <label className={styles.labelS}> Email </label>
+            <label
+              htmlFor="floating_password"
+              className="peer-focus:font-medium absolute text-sm text-pwgreen-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pwgreen-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Nombre
+            </label>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
             <input
-              className={styles.inputS}
-              placeholder="Let us know how to contact you back.."
+              type="text"
+              className="block py-2.5 px-0 w-full text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pwgreen-600 peer"
+              placeholder=" "
               {...register("email", { required: true, maxLength: 40 })}
             />
-          </p>
-          <p className={styles.pS}>
-            <label className={styles.labelS}> Message </label>
+            <label
+              htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-pwgreen-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pwgreen-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Su email
+            </label>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
             <input
-              className={styles.inputS}
-              placeholder="What would you like to tell us.."
+              type="text"
+              id=""
+              className="block py-2.5 px-0 w-full text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-pwgreen-600 peer"
+              placeholder=" "
               {...register("message", { required: true, maxLength: 20 })}
             />
-          </p>
-          <button className={styles.buttonS}>Send Message</button>
-          <div className={styles.divS}>
-            <span className={styles.spanS}></span> 001 1023 567
-            <span className={styles.spanS}></span> pawsitiveContact@gmail.com
+            <label
+              htmlFor="floating_repeat_password"
+              className="peer-focus:font-medium absolute text-sm text-pwgreen-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pwgreen-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Mensaje{" "}
+            </label>
+          </div>
+          <button type="submit" className="text-white bg-pwgreen-500 hover:bg-pwgreen-800 focus:ring-4 focus:outline-none focus:ring-pwgreen-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+            Enviar
+          </button>
+          <div className="md:absolute -bottom-0.5 right-2 bg-pwgreen-600 text-white w-80 px-8 rounded-md text-sm shadow-md shadow-pwgreen-700 sm:mt-24 " >
+            <span className="mx-8"></span> Email de contacto
+            <span className="mx-8"></span> pawsitiveteam0@gmail.com
           </div>
         </form>
       </div>
@@ -85,3 +114,4 @@ const Contact: NextComponentType = () => {
   );
 };
 export default Contact;
+
