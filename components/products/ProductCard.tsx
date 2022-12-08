0c4359d4f2127572c7b3a57704543569375f0b71
arbelais <arbelaism@outlook.com>
2022-12-08 20:52:56 -0300

    added button cart animation

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
-import { Product } from 'app/types';
 import Link from 'next/link'
+import { Product } from 'app/types';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import { FaShoppingCart, FaBox } from "react-icons/fa";
 
 type Props = {
     id: string
@@ -20,63 +21,71 @@ const ProductCard = ({ id, product, handleAddToCart }: Props) => {
     }
 
     return (
-        
-<div key={id} >
-    <div className="flex flex-col justify-between items-center m-5 bg-white shadow-md rounded-lg w-60 h-80 shadow-xl">
-        <div className='h-1/8'>
-            <Image className="rounded-t-lg p-8" src={product.photo} alt="product image" width={120} height={120}/>
-        </div>          
-        <div className="flex flex-col justify-between items-center h-7/8 z-1000">
-            <div className='px-5'>
-                <h3 className="text-gray-900 font-Rubik">
-                    {product.name}
-                </h3>
-            </div>
-            
-            <div className="flex items-center mb-2.5">
-                {stars.map((star, idx)=>{
-                    if(star===true){
-                        return <AiFillStar key={idx}/>
-                    } 
-                    if(star===false){
-                        return <AiOutlineStar key={idx}/>
-                    } 
-                })}
-            </div>
-            <div className='flex flex-row justify-center items-center mb-1 mt-1 font-Rubik'>
-                <div className='px-2.5'>
-                    <span className="text-lg text-pwpurple-700">
-                        ${product.displayPrice}
-                    </span>
+        <div key={id}>
+            <div className="flex flex-col justify-between items-center m-5 bg-white rounded-lg w-60 h-80 shadow-xl">
+                <div className="h-1/8">
+                    <Image
+                        className="rounded-t-lg p-8"
+                        src={product.photo}
+                        alt="product image"
+                        width={120}
+                        height={120}
+                    />
                 </div>
-                <div className='px-2.5'>
-                    <div>
-                        <button onClick={() => 
-                            handleAddToCart(product)} 
-                            disabled = {product.stock<=1 && true} 
-                            className="text-l font-bold rounded-lg px-1 py-1 border-2 border-pwpurple-700 text-pwpurple-700 hover:bg-pwpurple-700 hover:text-pwpurple-100 duration-300"
-                            >
-                            +Carrito
-                        </button>
+                <div className="flex flex-col justify-between items-center h-7/8 z-1000">
+                    <div className="px-5">
+                        <h3 className="text-gray-900 font-Rubik">
+                            {product.name}
+                        </h3>
+                    </div>
+
+                    <div className="flex items-center mb-2.5">
+                        {stars.map((star, idx) => {
+                            if (star === true) {
+                                return <AiFillStar key={idx} />
+                            }
+                            if (star === false) {
+                                return <AiOutlineStar key={idx} />
+                            }
+                        })}
+                    </div>
+                    <div className="flex flex-row justify-center items-center mb-1 mt-1 font-Rubik">
+                        <div className="px-2.5">
+                            <span className="text-lg text-pwpurple-700">
+                                ${product.displayPrice}
+                            </span>
+                        </div>
+                        <div className="px-2.5">
+                            <div>
+                                <button
+                                    onClick={() => handleAddToCart(product)}
+                                    disabled={product.stock <= 1 && true}
+                                    className="cart-button"
+                                    id={'buttonCart' + product.id}>
+                                    <span className="add-to-cart">
+                                        +Carrito
+                                    </span>
+                                    <span className="added">Agregado</span>
+                                    <span className="fa-shopping-cart">
+                                        <FaShoppingCart />
+                                    </span>
+                                    <span className="fa-box">
+                                        <FaBox />
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="pb-5">
+                        <Link href="/products/:id">
+                            <a className="text-pwgreen-500  text-md font-semibold">
+                                Ver detalles
+                            </a>
+                        </Link>
                     </div>
                 </div>
-
-            </div>
-            <div className='pb-5'>
-                <Link href="/products/:id">
-                    <a className="text-pwgreen-500  text-md font-semibold">
-                        Ver detalles
-                    </a>
-                </Link>
             </div>
         </div>
-    </div>
-
-</div>
-
-
-            
-        
     )
 }
 
