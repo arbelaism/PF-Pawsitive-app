65b3a5580eca438b38d3b26af1fd16bfab0aaa6d
Cristian Acevedo Posada <cris-a2112@hotmail.com>
2022-12-08 16:22:43 -0500

    contact-filters->tailwind

--- components/layout/Filters.tsx
+++ components/layout/Filters.tsx
@@ -89,52 +89,57 @@ const Filters = ({ setData, data, setCurrentPage }: Props) => {
     return;
   }
   return (
-    <div className={styles.filtersContainer}>
-      <div className={styles.itemFilter}>
-        <h2>Filtrar por Categoría</h2>
-        <select
-          name="breed"
-          onChange={(e) => handleOptions(e)}
-          className={styles.itemSelector}
-        >
-          {options.breed === "" &&  <option value="">Categoría...</option>}
-          <option value="gato">Gatos</option>
-          <option value="perro">Perros</option>
-          <option value="ave">Aves</option>
-          <option value="tortuga">Tortugas</option>
-        </select>
-      </div>
+    <div className="w-64 h-auto p-5 rounded-lg bg-pwgreen-600 mt-16 drop-shadow-lg shadow-lg shadow-pwpurple-200">
+      <div className="flex flex-col items-center w-[100%]  h-[25%] bg-pwgreen-600 ">
+        <div className="text-center p-1 mt-12">
+          <h2 className="text-white mb-2">Filtrar por Categoría</h2>
+          <select
+            name="breed"
+            onChange={(e) => handleOptions(e)}
+            className="bg-pwpurple-500 border-2 border-pwpurple-600 text-white text-sm rounded-lg focus:ring-pwpurple-700 focus:border-pwpurple-700 block w-full p-2.5"
+          >
+            {options.breed === "" && <option value="">Categoría...</option>}
+            <option className="" value="gato">Gatos</option>
+            <option value="perro">Perros</option>
+            <option value="ave">Aves</option>
+            <option value="tortuga">Tortugas</option>
+          </select>
+        </div>
 
-      <div className={styles.itemFilter}>
-        <h2>Filtrar por Tamaño</h2>
-        <select
-          name="size"
-          onChange={(e) => handleOptions(e)}
-          className={styles.itemSelector}
-        >
-          {options.size === "" && <option value="">Tamaño...</option>}
-          <option value="BIG">Grande</option>
-          <option value="MEDIUM">Mediano</option>
-          <option value="SMALL">Pequeño</option>
-        </select>
-      </div>
+        <div className="flex flex-col items-center w-[100%]  h-[25%] bg-pwgreen-600 mt-8">
+          <h2 className="text-white mb-2">Filtrar por Tamaño</h2>
+          <select
+            name="size"
+            onChange={(e) => handleOptions(e)}
+            className="bg-pwpurple-500 border-2 border-pwpurple-600 text-white text-sm rounded-lg focus:ring-pwpurple-700 focus:border-pwpurple-700 block w-full p-2.5"
+          >
+            {options.size === "" && <option value="">Tamaño...</option>}
+            <option value="BIG">Grande</option>
+            <option value="MEDIUM">Mediano</option>
+            <option value="SMALL">Pequeño</option>
+          </select>
+        </div>
 
-      <div className={styles.itemFilter}>
-        <h2>Filtrar por Edad</h2>
-        <select
-          name="age"
-          onChange={(e) => handleOptions(e)}
-          className={styles.itemSelector}
-        >
-          {options.age === "" && <option value="">Edad...</option>} 
-          <option value="min">Menor a mayor</option>
-          <option value="max">Mayor a menor</option>
-        </select>
-      </div>
-      <div>
-        <button className={styles.buttons} onClick={handleReset}>
-          Reset
-        </button>
+        <div className="flex flex-col items-center w-[100%]  h-[25%] bg-pwgreen-600 mt-16">
+          <h2 className="text-white mb-2">Filtrar por Edad</h2>
+          <select
+            name="age"
+            onChange={(e) => handleOptions(e)}
+            className="bg-pwpurple-500 border-2 border-pwpurple-600 text-white text-sm rounded-lg focus:ring-pwpurple-700 focus:border-pwpurple-700 block w-full p-2.5"
+          >
+            {options.age === "" && <option value="">Edad...</option>}
+            <option value="min">Menor a mayor</option>
+            <option value="max">Mayor a menor</option>
+          </select>
+        </div>
+        <div className="flex flex-col items-center w-[50%]  h-[25%] bg-pwgreen-600 mt-24 rounded-xl">
+          <button
+            className="bg-pwpurple-500 border-2 border-pwpurple-600 text-white text-sm rounded-lg focus:ring-pwpurple-700 focus:border-pwpurple-700 block w-full p-2.5"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
