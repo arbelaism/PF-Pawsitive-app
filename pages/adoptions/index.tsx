ab710fc1c3892adaf55f81dcaa11f4e5efbc7271
ferdev2712 <ferdev2712@gmail.com>
2022-12-09 11:56:52 -0400

    bug fixed

--- pages/adoptions/index.tsx
+++ pages/adoptions/index.tsx
@@ -38,7 +38,7 @@ const Adoptions: NextPage = () => {
     }, [isSuccess, adoptions])
 
     return (
-        <MainLayout title="Pawsitive - Adopciones">\
+        <MainLayout title="Pawsitive - Adopciones">
             <div className="px-4 py-2 w-full flex justify-between items-center bg-pwgreen-100 mt-4">
                 <h1 className="text-3xl font-bold p-4 bg-pwpurple-50 rounded-xl border-pwgreen-700 shadow-xl lg:text-5xl">Adopciones</h1>
                 <Link href={'/create'}>
