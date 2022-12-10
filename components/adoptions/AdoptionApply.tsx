e4101bc178d15de6ce53a64770f9b75852cb0bc3
Nestor <ndqa96@gmail.com>
2022-12-10 18:01:12 -0400

    form is already functional

--- components/adoptions/AdoptionApply.tsx
+++ components/adoptions/AdoptionApply.tsx
@@ -1,70 +1,43 @@
 import { NextComponentType } from 'next';
-import { useState } from 'react';
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { useQueryClient } from 'react-query';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useMutation } from 'react-query'
+import {apply} from 'utils/dbFetching';
+import { Form } from 'app/types';
 import Image from 'next/image'
-import Link from 'next/link'
 import IsoGreen from 'public/iso-green.svg'
-import { type } from 'os';
 
 const AdoptionApply: NextComponentType = () => {
 
-    const { user, error: err, isLoading: loading } = useUser();
-    //De momento se usa un USERID hardcodeado, hasta que se modifique el model correspondiente a los usuarios. Arriba, se ve como se accede
-//user que se necesita.
-    const queryClient = useQueryClient();   
-    console.log(queryClient.getQueriesData('pet')); //Manda undefined, tanto por queries como por caché. No puede accederse desde otro componente al caché
+    //TODO: 
+    //==>> RESET LUEGO DE SUBMIT.
+    //==>> ENVIAR MAIL INTERNO Y AL CLIENTE UNA VEZ HECHO EL SUBMIT.
+    //==>> EVUALUAR QUE NO SE PUEDA HACER MAS DE UNA APLICACION A ADOPCION
+    //==>> MOSTRAR NOTIFICACION DE SUCCESS O FAILURE
 
-    const [data, setData] = useState({                
-        reason: "",
-        past: "",
-        residence: "",
-        employee: "",
-        garden: "",
-        adoptionPostId: "",
-        userId: ""
+    const { mutate } = useMutation(apply);
+    const ids = JSON.parse(window.localStorage.ids);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<Form>({
+            defaultValues:{
+                reason:'',
+                past: '',
+                residence:'',
+                employee:'',
+                garden:'',
+                adoptionPostId: ids.userId,
+                userId: ids.petId
+            }
     });
-    const [errores, setErrores] = useState({});
-    
-    interface Obj{                
-        reason: "",
-        past: "",
-        residence: "",
-        employee: "",
-        garden: "",
-        adoptionPostId: "",
-        userId: ""
+
+    const onSubmit: SubmitHandler<Form> = async(data)=> {
+        data = { ...data}
+        mutate(data)
+        console.log(data)
+        alert('TODO')
     }
-    function validate(obj:Obj){
-        let error={}; //Objeto en que se almacenarán los errores.
-        //Valida que exista el nombre
-        if(obj.reason.length <= 1 || typeof obj.reason !== "string") error.reason = "Debes ingresar una descripción";
-        //Valida que exista la descripcion
-        if(obj.past === "") error.past = "Debes indicar si has tenido una mascota anteriormente";
-        //Valida que exista la URL para la imagen del juego a crear
-        if(obj.Img_URL.length <=1 || typeof obj.Img_URL !== "string") error.Img_URL = "Debes ingresar una URL para tu imagen";
-        //Valida que se haya ingresado al menos una plataforma
-        if(obj.Plataformas.length <1) error.Plataformas = "Debes ingresar al menos una plataforma";
-        //Valida que exista un arreglo con al menos un género
-        if(obj.Generos.length <1) error.Generos = "Debes ingresar al menos un género";
-        //Valida que el rating ingresado sea menor o igual a cinco, y mayor o igual a 1
-        if(obj.Rating > 5) error.Rating = "El Rating debe ser igual o menor a 5";
-        if(obj.Rating < 1) error.Rating = "El Rating debe ser igual o mayor a 1";
-        
-        
-        return error;
-};
-function handleChange(e){
-    setErrores(validate(data));
-    setData({
-            ...data, //Pushea al obj solo lo que se indique segun sea el input
-            [e.target.name]:e.target.value
-    });
-   
-
-}
-
-
 
     return (
         <div className="p-8 flex flex-col justify-evenly items-center bg-pwgreen-100 rounded-lg md:flex-row">
@@ -83,62 +56,138 @@ function handleChange(e){
                     aplicar a adoptar ésta mascota.
                 </p>
             </div>
-            <form className="w-full grid grid-cols-1 gap-2 items-center justify-center md:w-2/6 lg:gap-3">
+            <form className="w-full grid grid-cols-1 gap-2 items-center justify-center md:w-2/6 lg:gap-3"
+                onSubmit={handleSubmit(onSubmit)}
+            >
                 <div className="flex flex-col gap-1 items-start justify-center">
                     <label htmlFor="reason" className="label">
                         ¿Por qué quieres adoptar ésta mascota?
                     </label>
                     <textarea
-                        name="reason"
                         placeholder="Escribe aquí la razón por la cual quieres adoptar a esta Paw-Mascota."
                         className="input"
+                        {...register('reason', {
+                            required: true,
+                            minLength: 50,
+                            pattern: /[a-zA-Z\s:]/
+                        })}
                     />
+                        {(errors.reason?.type === 'required' && (
+                            <span className="text-red-500 text-xs">
+                                Debes completar éste campo.
+                            </span>
+                            )) ||
+                        (errors.reason?.type === 'minLength' && (
+                            <span className="text-red-500 text-xs">
+                                Detalla con al menos 50 caracteres porqué quieres adoptar esta mascota.
+                            </span>
+                            ))
+                        }
                 </div>
                 <div className="flex gap-1 flex-col items-start justify-center">
                     <label htmlFor="residence" className="label">
                         ¿Qué tipo de vivienda tienes?
                     </label>
                     <select
-                        name="residence"
-                        value=""
                         className="input"
+                        {...register('residence', {
+                            required: true,
+                            minLength: 2,
+                            pattern: /[a-zA-Z\s:]/
+                        })}
                     >
-                        <option value="">Vivienda...</option>
+                        <option value="" >Vivienda...</option>
                         <option value="DEPARTAMENTO">Departamento</option>
                         <option value="CASA">Casa</option>
                         <option value="PISO">Piso</option>
 
                     </select>
+                    {
+                        errors.residence?.type === 'required' ?
+                            <span className="text-red-500 text-xs">
+                                Por favor, indica tu tipo de vivienda.
+                            </span>
+                            :
+                            null
+                    }
+                    
                 </div>
                 <div className="flex gap-1 flex-col items-start justify-center">
                     <label htmlFor="employee" className="label">
                         ¿Tienes empleo?
                     </label>
                     <select
-                        name="employee"
-                        value=""
                         className="input"
+                        {...register('employee', {
+                            required: true,
+                            minLength: 2,
+                            setValueAs: (v) => Boolean(v)
+                        })}
                     >
-                        <option value="">Empleo...</option>
+                        <option value="" >Empleo...</option>
                         <option value='true'>Si tengo empleo</option>
                         <option value='false'>No tengo empleo</option>
                     </select>
+                    {
+                        errors.employee?.type === 'required' ?
+                            <span className="text-red-500 text-xs">
+                                Por favor, indica si cuentas con empleo.
+                            </span>
+                            :
+                            null
+                    }
                 </div>
                 <div className="w-full flex gap-1 flex-col items-start justify-center">
                     <label htmlFor="garden" className="label">
                         ¿Tienes jardín o patio?
                     </label>
                     <select
-                        name="garden"
-                        value=""
                         className="input"
+                        {...register('garden', {
+                            required: true,
+                            minLength: 2,
+                            pattern: /[a-zA-Z\s:]/
+                        })}
                     >
-                        <option value="">Jardín o patio...</option>
+                        <option value="" >Jardín o patio...</option>
                         <option value='true'>Si, tengo jardín y/o patio.</option>
                         <option value='false'>No, no tengo jardín y/o patio.</option>
                     </select>
+                    {
+                        errors.garden?.type === 'required' ?
+                            <span className="text-red-500 text-xs">
+                                Por favor, indica si tienes jardín o patio.
+                            </span>
+                            :
+                            null
+                    }
                 </div>
-                <button className="text-center bg-pwgreen-500 py-3 my-2 rounded-md shadow-xl text-pwgreen-900 font-bold uppercase font-Rubik">
+                <div className="w-full flex gap-1 flex-col items-start justify-center">
+                    <label htmlFor="past" className="label">
+                        ¿Anteriormente has tenido alguna mascota?
+                    </label>
+                    <select
+                        className="input"
+                        {...register('past', {
+                            required: true,
+                            minLength: 2,
+                            pattern: /[a-zA-Z\s:]/
+                        })}
+                    >
+                        <option value="" >Mascota...</option>
+                        <option value='true'>Si, he tenido una o más mascotas.</option>
+                        <option value='false'>No, no he tenido mascotas.</option>
+                    </select>
+                    {
+                        errors.past?.type === 'required' ?
+                            <span className="text-red-500 text-xs">
+                                Por favor, si ya has tenido mascotas.
+                            </span>
+                            :
+                            null
+                    }
+                </div>
+                <button type='submit' className="text-center bg-pwgreen-500 py-3 my-2 rounded-md shadow-xl text-pwgreen-900 font-bold uppercase font-Rubik">
                     SOLICITAR ADOPCIÓN
                 </button>
                 {/* <div className="flex justify-center items-center">
