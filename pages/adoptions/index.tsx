cb5ffd82c8545c47155b632b45c462806ec18b2a
nestordqa <ndqa96@gmail.com>
2022-12-14 05:56:26 +0100

    route modified

--- pages/adoptions/index.tsx
+++ pages/adoptions/index.tsx
@@ -51,7 +51,7 @@ const Adoptions: NextPage = () => {
         })
       }
       else{
-        router.push('/adoptions/create')
+        router.push('/adoptions/post')
       }
     }
 
@@ -62,73 +62,79 @@ const Adoptions: NextPage = () => {
     }, [isSuccess, adoptions])
 
     return (
-        <MainLayout title="Pawsitive - Adopciones">
-            <div className="px-4 py-2 w-full flex justify-between items-center bg-pwgreen-100 mt-4">
-                <h1 className="text-3xl font-bold p-4 bg-pwpurple-50 rounded-xl border-pwgreen-700 shadow-xl lg:text-5xl">Adopciones</h1>
+        <MainLayout title="Pawsitive - Adopciones">          
+          <div className="px-4 py-2 w-full flex justify-between items-center bg-pwgreen-100 mt-4">
+                <h1 className="text-3xl font-bold p-4 bg-pwpurple-50 rounded-xl border-pwgreen-700 shadow-xl lg:text-5xl">
+                  Adopciones
+                </h1>
+                <Link href="/adoptions/post">
                 <button onClick={alertAdoptionForm}>
                 <a
-            href="#"
-            className="inline-flex justify-center items-center p-5 text-base font-medium text-white bg-pwgreen-700 rounded-lg hover:text-white hover:bg-pwpurple-800 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <svg
-              aria-hidden="true"
-              className="mr-3 w-5 h-5"
-              viewBox="0 0 22 31"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clipPath="url(#clip0_4151_63004)">
-                <path
-                  d="M5.50085 30.1242C8.53625 30.1242 10.9998 27.8749 10.9998 25.1035V20.0828H5.50085C2.46546 20.0828 0.00195312 22.332 0.00195312 25.1035C0.00195312 27.8749 2.46546 30.1242 5.50085 30.1242Z"
-                  fill="#58c184"
-                />
-                <path
-                  d="M0.00195312 15.062C0.00195312 12.2905 2.46546 10.0413 5.50085 10.0413H10.9998V20.0827H5.50085C2.46546 20.0827 0.00195312 17.8334 0.00195312 15.062Z"
-                  fill="#3ea76a"
-                />
-                <path
-                  d="M0.00195312 5.02048C0.00195312 2.24904 2.46546 -0.000244141 5.50085 -0.000244141H10.9998V10.0412H5.50085C2.46546 10.0412 0.00195312 7.79193 0.00195312 5.02048Z"
-                  fill="#308253"
-                />
-                <path
-                  d="M11 -0.000244141H16.4989C19.5343 -0.000244141 21.9978 2.24904 21.9978 5.02048C21.9978 7.79193 19.5343 10.0412 16.4989 10.0412H11V-0.000244141Z"
-                  fill="#823060"
-                />
-                <path
-                  d="M21.9978 15.062C21.9978 17.8334 19.5343 20.0827 16.4989 20.0827C13.4635 20.0827 11 17.8334 11 15.062C11 12.2905 13.4635 10.0413 16.4989 10.0413C19.5343 10.0413 21.9978 12.2905 21.9978 15.062Z"
-                  fill="#5d2344"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_4151_63004">
-                  <rect
-                    width="22"
-                    height="30.1244"
-                    fill="white"
-                    transform="translate(0 -0.000244141)"
-                  />
-                </clipPath>
-              </defs>
-            </svg>
-            <span className="w-full">
-              Poner en adopción
-            </span>
-            <svg
-              aria-hidden="true"
-              className="ml-3 w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a>
+                  className="inline-flex justify-center items-center p-5 text-base font-medium text-white bg-pwgreen-700 rounded-lg hover:text-white hover:bg-pwpurple-800 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                <svg
+                  aria-hidden="true"
+                  className="mr-3 w-5 h-5"
+                  viewBox="0 0 22 31"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clipPath="url(#clip0_4151_63004)">
+                    <path
+                      d="M5.50085 30.1242C8.53625 30.1242 10.9998 27.8749 10.9998 25.1035V20.0828H5.50085C2.46546 20.0828 0.00195312 22.332 0.00195312 25.1035C0.00195312 27.8749 2.46546 30.1242 5.50085 30.1242Z"
+                      fill="#58c184"
+                    />
+                    <path
+                      d="M0.00195312 15.062C0.00195312 12.2905 2.46546 10.0413 5.50085 10.0413H10.9998V20.0827H5.50085C2.46546 20.0827 0.00195312 17.8334 0.00195312 15.062Z"
+                      fill="#3ea76a"
+                    />
+                    <path
+                      d="M0.00195312 5.02048C0.00195312 2.24904 2.46546 -0.000244141 5.50085 -0.000244141H10.9998V10.0412H5.50085C2.46546 10.0412 0.00195312 7.79193 0.00195312 5.02048Z"
+                      fill="#308253"
+                    />
+                    <path
+                      d="M11 -0.000244141H16.4989C19.5343 -0.000244141 21.9978 2.24904 21.9978 5.02048C21.9978 7.79193 19.5343 10.0412 16.4989 10.0412H11V-0.000244141Z"
+                      fill="#823060"
+                    />
+                    <path
+                      d="M21.9978 15.062C21.9978 17.8334 19.5343 20.0827 16.4989 20.0827C13.4635 20.0827 11 17.8334 11 15.062C11 12.2905 13.4635 10.0413 16.4989 10.0413C19.5343 10.0413 21.9978 12.2905 21.9978 15.062Z"
+                      fill="#5d2344"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_4151_63004">
+                      <rect
+                        width="22"
+                        height="30.1244"
+                        fill="white"
+                        transform="translate(0 -0.000244141)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+                
+                  <span className="w-full">
+                    Poner en adopción
+                  </span>
+                
+                <svg
+                  aria-hidden="true"
+                  className="ml-3 w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
                 </button>
+                </Link>
             </div>
+          
 
             <div className="flex">
                 <div className="flex grow flex-col justify-center items-center bg-transparent">
