4f666dae4fd4f5e33dc0f2207e47cb7860e99a3d
nestordqa <ndqa96@gmail.com>
2022-12-14 05:52:20 +0100

    adoption form post

---
+++ pages/adoptions/post/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next'
+import { MainLayout } from 'components'
+import AdoptionForm from 'components/adoptions/AdoptionForm';
+
+const Apply: NextPage = () => {
+    
+
+    return (
+        <MainLayout title="Pawsitive - Adoptions Apply">
+            <AdoptionForm/>
+        </MainLayout>
+    )
+}
+
+export default Apply;
