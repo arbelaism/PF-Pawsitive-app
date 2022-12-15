658124ace0ffed08a8dc825d7eb4b904f5e08b67
arbelais <arbelaism@outlook.com>
2022-12-14 21:26:17 -0300

    some adjustements

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Image from 'next/image'
-import pretitleImg from '../../public/img/hero/pretitle-img.svg'
+import pretitleImg from '../../public/img/hero/pretitle-img-green.svg'
 import Link from 'next/link'
 
 const Hero = () => {
   return (
-    <section className='bg-pwgreen-100 lg:bg-hero lg:bg-cover lg:bg-no-repeat min-h-[400px] lg:min-h-[805px]'>
-      <div className='container mx-auto flex justify-start items-center min-h-[400px] lg:h-[805px]'>
-        <div className='lg:max-w-[650px] text-center mx-auto lg:text-left lg:mx-0'>
+    <section className='bg-pwgreen-100 lg:bg-hero lg:bg-cover lg:bg-no-repeat h-max lg:h-screen'>
+      <div className='container mx-auto flex justify-start items-center min-h-[400px] lg:h-full'>
+        <div className='lg:max-w-[650px] text-left mx-auto lg:mx-0'>
           <div className='hidden xl:flex mb-6 ml-5'>
             <Image
             src={pretitleImg}
@@ -15,7 +15,7 @@ const Hero = () => {
             alt=''
             />
           </div>
-          <h1 className='text-5xl lg:text-8xl uppercase font-bold -tracking-[0.05em] mb-10'>Un primer<br /> <span className='text-pwgreen-500 font-normal'>acercamiento de la mascota</span> <br /> al bienestar</h1>
+          <h1 className='font-Rubik text-5xl lg:text-7xl 2xl:text-8xl uppercase font-bold -tracking-[0.05em] mb-10'>Un primer<br /> <span className='text-pwgreen-500 font-normal'>acercamiento de la mascota</span> <br /> al bienestar</h1>
           <Link href={'/adoptions'}>
             <button className='h-[62px] flex justify-center items-center py-3 px-8 rounded-full text-[20px] transition-all bg-pwgreen-300 text-pwgreen-50 hover:bg-pwgreen-500 mx-auto lg:mx-0'>Ver mas</button>
           </Link>
--- components/home/Pets.tsx
+++ components/home/Pets.tsx
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
 
 import Pet1Img from 'public/img/pets/1.png'
 import Pet2Img from 'public/img/pets/2.png'
@@ -14,120 +14,168 @@ import Pet10Img from 'public/img/pets/10.png'
 import Pet11Img from 'public/img/pets/11.png'
 import Pet12Img from 'public/img/pets/12.png'
 import Badge from 'public/img/pets/badge.svg'
+import { useQuery } from 'react-query'
+import { getAdoptions } from 'utils/dbFetching'
+import { IAdoption } from 'app/types'
+import Link from 'next/link'
 
 const pets = [
-  {
-    id:1,
-    category: 'gato',
-    name: 'kyba',
-    image: Pet1Img
-  },
-  {
-    id:2,
-    category: 'pajaro',
-    name: 'rotty',
-    image: Pet2Img
-  },
-  {
-    id:4,
-    category: 'gato',
-    name: 'kirra',
-    image: Pet4Img
-  },
-  {
-    id:5,
-    category: 'hamster',
-    name: 'dinno',
-    image: Pet5Img
-  },
-  {
-    id:6,
-    category: 'pajaro',
-    name: 'paco',
-    image: Pet6Img
-  },
-  {
-    id:7,
-    category: 'pajaro',
-    name: 'pavo',
-    image: Pet7Img
-  },
-  {
-    id:8,
-    category: 'gato',
-    name: 'ynna',
-    image: Pet8Img
-  },
-  {
-    id:10,
-    category: 'pajaro',
-    name: 'dimmi',
-    image: Pet10Img
-  },
-  {
-    id:11,
-    category: 'perro',
-    name: 'jasper',
-    image: Pet11Img
-  },
-  {
-    id:12,
-    category: 'hamster',
-    name: 'syu',
-    image: Pet12Img
-  }
+    {
+        id: 1,
+        category: 'gato',
+        name: 'kyba',
+        photo: Pet1Img
+    },
+    {
+        id: 2,
+        category: 'pajaro',
+        name: 'rotty',
+        image: Pet2Img
+    },
+    {
+        id: 4,
+        category: 'gato',
+        name: 'kirra',
+        image: Pet4Img
+    },
+    {
+        id: 5,
+        category: 'hamster',
+        name: 'dinno',
+        image: Pet5Img
+    },
+    {
+        id: 6,
+        category: 'pajaro',
+        name: 'paco',
+        image: Pet6Img
+    },
+    {
+        id: 7,
+        category: 'pajaro',
+        name: 'pavo',
+        image: Pet7Img
+    },
+    {
+        id: 8,
+        category: 'gato',
+        name: 'ynna',
+        image: Pet8Img
+    },
+    {
+        id: 10,
+        category: 'pajaro',
+        name: 'dimmi',
+        image: Pet10Img
+    },
+    {
+        id: 11,
+        category: 'perro',
+        name: 'jasper',
+        image: Pet11Img
+    },
+    {
+        id: 12,
+        category: 'hamster',
+        name: 'syu',
+        image: Pet12Img
+    }
 ]
 
 const Pets = () => {
-  const [petDetails, setPetDetails] = useState(pets[8])
-  const [petIndex, setPetIndex] = useState(8)
-  const getPetDetails = (id:any) =>{
-    const pet:any = pets.find((pet)=>{
-      return pet.id === id
-    })
-    setPetDetails(pet)
-  }
+    const {
+        data: adoptions,
+        error,
+        isLoading,
+        isSuccess
+    } = useQuery(['adoptions'], getAdoptions)
 
-  return (
-    <section className='bg-pets bg-center py-6 overflow-hidden'>
-      <div className='flex flex-col lg:flex-row'>
-        <div className='hidden xl:flex xl:w-[30%] xl:pl-[160px]'>
-          <Image 
-          src={Badge}
-          width={230}
-          height={227}
-          alt=''
-          />
-        </div>
-        <div className='flex-1 flex flex-col lg:flex-row'>
-          <div className='lg:w-[30%] flex flex-col justify-center items-end pb-6 lg:py-2 mx-auto lg:mx-0 '>
-            <div className='text-center text-pwgreen-50'>
-              <div className='text-[32px] capitalize'>{petDetails.category}</div>
-              <div className='text-[17px] uppercase mb-1'>({petDetails.name})</div>
-              <div className='w-[150px] h-[150px] mx-auto lg:mx-0 border-4 border-white rounded-full'>
-                <Image src={petDetails.image} width={150} height={150} alt='no hay imagen que mostrar'/>
-              </div>
-            </div>
-          </div>
-          <div className='relative lg:w-[60%] flex-1 lg:flex items-center'>
-            <div className='flex flex-wrap gap-4 justify-center lg:justify-end lg:-mr-4'>
-              {pets.map((pet,index) => {
-                return (
-                <div key={index} onClick={()=>{setPetIndex(index), getPetDetails(pet.id)}} className='cursor-pointer relative'>
-                  <div className={`w-full h-full absolute rounded-full ${petIndex === index ? 'border-2 border-white':'bg-black/40'}`}></div>
-                  <Image
-                  src={pet.image}
-                  width={95}
-                  height={95}
-                  alt=''/>
-                </div>)
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-};
+    let data: any = []
+    if (!isLoading) data = adoptions.slice(0, 13)
 
-export default Pets;
+    const [petDetails, setPetDetails] = useState(pets[0])
+    const [petIndex, setPetIndex] = useState(8)
+    const getPetDetails = (id: any) => {
+        const pet: any = data.find((pet: any) => {
+            return pet.id === id
+        })
+
+        setPetDetails(pet)
+    }
+
+    return (
+        <section className="bg-pets bg-center py-12 overflow-hidden relative">
+            <div className="flex flex-col lg:flex-row">
+                <div className="hidden xl:flex xl:w-[30%] xl:pl-[160px]">
+                    {/* <Image  */}
+                    {/* src={Badge} */}
+                    {/* width={230} */}
+                    {/* height={227} */}
+                    {/* alt='' */}
+                    {/* /> */}
+                    <h1 className="title">Algunas de nuestras mascotas</h1>
+                </div>
+                {isLoading ? (
+                    <h1>Loading</h1>
+                ) : (
+                    <div className="flex-1 flex flex-col lg:flex-row">
+                        <div className="lg:w-[30%] flex flex-col justify-center items-end pb-6 lg:py-2 mx-auto lg:mx-0 ">
+                            <div className="text-center text-pwgreen-50">
+                                <div className="text-[17px] font-bold uppercase mb-1">
+                                    {petDetails?.name}
+                                </div>
+                                <div className="w-[150px] h-[150px] mx-auto lg:mx-0 border-4 border-white rounded-full overflow-hidden">
+                                    <Image
+                                        src={petDetails.photo || ''}
+                                        objectFit={'cover'}
+                                        width={150}
+                                        height={150}
+                                        alt="no hay imagen que mostrar"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="relative lg:w-[60%] flex-1 lg:flex items-center">
+                            <div className="flex flex-wrap gap-4 justify-center lg:justify-end lg:mr-4">
+                                {data.map((pet: any, index: any) => {
+                                    return (
+                                        <div
+                                            key={index}
+                                            onClick={() => {
+                                                setPetIndex(index),
+                                                    getPetDetails(pet.id)
+                                            }}
+                                            className="cursor-pointer relative overflow-hidden rounded-full">
+                                            <div
+                                                className={`w-full h-full absolute rounded-full ${
+                                                    petIndex === index
+                                                        ? 'border-2 border-white'
+                                                        : 'bg-black/40'
+                                                }`}></div>
+                                            <Image
+                                                src={pet.photo}
+                                                objectFit={'cover'}
+                                                width={80}
+                                                height={80}
+                                                alt=""
+                                            />
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+            <div className="text-end absolute right-2 bottom-2">
+                <Link href="/adoptions">
+                    <button className="font-Rubik uppercase text-base font-bold px-3 py-2 text-white hover:text-pwgreen-900 duration-300 lg:text-xl lg:py-4 lg:px-5">
+                        Ver más
+                    </button>
+                </Link>
+            </div>
+        </section>
+    )
+}
+
+export default Pets
