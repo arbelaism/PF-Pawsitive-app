711b96f0276cec7347fe6758bf789a89da055d54
nestordqa <ndqa96@gmail.com>
2022-12-15 07:38:43 +0100

    user profile

--- components/dashboard/dashboardUser/profile.tsx
+++ components/dashboard/dashboardUser/profile.tsx
@@ -1,330 +1,124 @@
 import { NextComponentType } from 'next';
-import React, { useState } from "react";
-import { useForm, SubmitHandler } from 'react-hook-form';
+import React from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { mediaUploader } from "utils/mediaUploader";
-import { useRouter } from "next/router"
-import axios from 'axios';
+import Link from 'next/link';
 import Image from 'next/image'
-import IsoGreen from 'public/iso-green.svg'
+import IsoGreen from 'public/iso-green.svg';
+import { useQuery } from 'react-query';
+import {getUserById} from 'utils/dbFetching';
 
 const MyProfile: NextComponentType = () => {
-    interface UserUpdate {};
 
-    const { user, error: err, isLoading: loading } = useUser();    
-    const [media, setMedia] = useState<File[]>([]);
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        const target = e.target as HTMLInputElement;
-        const files = [...Object.values(target.files!)];
-        setMedia([...files]);
-      };
+    const { user, isLoading: isLoadingU } = useUser();
 
     const {
-        register,
-        reset,
-        handleSubmit,
-        formState: { errors }
-    } = useForm({});
- 
-    const onSubmit: SubmitHandler<UserUpdate> = async(data)=> {
-        let urlPhoto: any = []
-        if (media.length > 0) {
-            urlPhoto = await mediaUploader(media);
-        }
-        if(urlPhoto !== null || urlPhoto !== undefined){
-            data = {...data,
-                photo: urlPhoto ? urlPhoto[0] : null
-            };
-        }
-        if(urlPhoto == null || urlPhoto == undefined){
-            data = {...data}
-        }
-
-        axios.put(`/api/user/${user?.sub}`, data)
-            .then(response => {
-                 console.log("Update SUCCESS!")
-            }).catch(error => {
-                console.log(error)
-                })
-        reset({})
-        alert('Datos actualizados!')
-    }
+        data: userProfile,
+        error,
+        isLoading,
+        isSuccess,
+      } = useQuery(["userProfile", user?.sub], () => getUserById(user?.sub));
+      let photo: string;
+      if(!isLoading){
+        console.log(userProfile);
+        photo = userProfile?.photo
+      }
 
     return (
         <div className="p-8 flex flex-col justify-evenly items-center bg-pwgreen-100 rounded-lg md:flex-row">
-            <div className="w-full flex flex-col gap-3 justify-center items-center md:w-3/6">
-                <Image
-                    src={IsoGreen}
-                    alt="not found"
-                    width={150}
-                    height={150}
-                />
-                <h2 className="font-Rubik font-bold text-4xl lg:text-6xl">
-                    ¡Adoptar!
-                </h2>
+            <div className="w-full flex flex-col gap-3 justify-center items-center md:w-1/3">
+                {
+                    isLoading ?
+                    <>
+                        <Image
+                            src={IsoGreen}
+                            alt="not found"
+                            width={150}
+                            height={150}
+                        />
+                        <h1>Espera un momento, estamos cargando tus datos...</h1>
+                    </>
+                    :
+                    <>
+                        <div className='flex flex-col justify-center items-center w-60 h-60 bg-pwpurple-100 rounded-full'>
+                            <Image
+                                src={userProfile?.photo}
+                                alt="not found"
+                                width={200}
+                                height={200}
+                                className="rounded-full static"
+                            />
+                        </div>
+                        
+                    </>
+                }
+                <div className="font-Rubik font-bold text-4xl lg:text-6xl">
+                    {isLoading ?
+                    'Mi perfil...'
+                    :
+                    <>
+                    <h2 className='text-1x4'>Hola, <span className='font-bold'>{userProfile?.firstName}</span></h2>
+                    </>
+                        
                 
-                <p className="text-md mb-2 md:text-xl md:text-center">
-                    Completa todos los datos de éste formulario para poder 
-                    aplicar a adoptar ésta mascota.
+                }
+                </div>
+                
+                <p className="text-md text-center mb-2 md:text-xl md:text-center">
+                    En este apartado podrás ver todos tus datos, si deseas actualizarlos o modificarlos, presiona el botón <span className='font-bold'>&quot;ACTUALIZAR DATOS&quot;</span>.
                 </p>
             </div>
 
-            <form className="w-full grid grid-cols-1 gap-2 items-center justify-center md:w-2/6 lg:gap-3"
-                onSubmit={handleSubmit(onSubmit)}
-            >
+            <div className="w-full grid grid-cols-1 gap-2 items-center justify-center md:w-2/3 lg:gap-3">
 
-                {/* NOMBRE */}
+                {isLoading ?
                 <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="firstName" className="label">
-                        Nombre:
-                    </label>
-                    <input
-                        placeholder="Nombre..."
-                        className="input"
-                        {...register('firstName', {
-                            maxLength: 20,
-                            pattern: /[a-zA-Z\s:]/
-                        })}
-                    />
-                    {
-                        errors.firstName?.type === 'maxLength' ?
-                        <span className="text-red-500 text-xs">
-                            El nombre no puede tener más de 20 caracteres.
-                        </span>
-                        :
-                        null                        
-                    }
-                    {
-                        errors.firstName?.type === 'pattern' ?
-                        <span className="text-red-500 text-xs">
-                            El nombre no puede contener caracteres especiales.
-                        </span>
-                        :
-                        null                        
-                    }
+                    <h1>Cargando usuario...</h1>
                 </div>
+                :
+                <>
+                    
+                    <div className='w-3/4 p-5 flex flex-col justify-end'>
+                        <div className="input">
+                            <h2><span className='font-bold'>Nombre y apellido: </span>{`${userProfile?.firstName} ${userProfile?.lastName}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>Email: </span>{`${userProfile?.email}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>Género: </span>{`${userProfile?.gender}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>País: </span>{`${userProfile?.country}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>Provincia: </span>{`${userProfile?.province}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>Ciudad: </span>{`${userProfile?.city}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>Dirección: </span>{`${userProfile?.address}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>Código postal: </span>{`${userProfile?.postCode}`}</h2>
+                        </div>
+                        <div className="input">
+                            <h2><span className='font-bold'>Teléfono: </span>{`${userProfile?.phone}`}</h2>
+                        </div>
+                    </div>
 
-                {/* APELLIDO */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="lastName" className="label">
-                        Apellido:
-                    </label>
-                    <input
-                        placeholder="Apellido..."
-                        className="input"
-                        {...register('lastName', {
-                            maxLength: 20,
-                            pattern: /[a-zA-Z\s:]/
-                        })}
-                    />
-                    {
-                        errors.lastName?.type === 'maxLength' ?
-                        <span className="text-red-500 text-xs">
-                            El apellido no puede tener más de 20 caracteres.
-                        </span>
-                        :
-                        null                        
-                    }
-                    {
-                        errors.lastName?.type === 'pattern' ?
-                        <span className="text-red-500 text-xs">
-                            El apellido no puede contener caracteres especiales.
-                        </span>
-                        :
-                        null                        
-                    }
-                </div>
+                    <Link href={'/dashboard/myprofile/updateuser'}>
+                        <button className="text-center bg-pwgreen-500 py-3 my-2 rounded-md shadow-xl text-pwgreen-900 font-bold uppercase font-Rubik">
+                            ACTUALIZAR DATOS
+                        </button>   
+                    </Link>
+             
+                </>
 
-                {/* EMAIL */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="email" className="label">
-                        Email:
-                    </label>
-                    <input
-                        placeholder="Email..."
-                        className="input"
-                        {...register('email', {
-                               pattern: /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
-                        })}
-                    />
-                    {
-                        errors.email?.type === 'pattern' ?
-                        <span className="text-red-500 text-xs">
-                            El correo electrónico ingresado no es válido.
-                        </span>
-                        :
-                        null                        
-                    }
-                </div>
 
-                {/* GENERO */}
-                <div className="flex gap-1 flex-col items-start justify-center">
-                    <label htmlFor="gender" className="label">
-                        Género:
-                    </label>
-                    <select
-                        className="input"
-                        {...register('gender')}
-                    >
-                        <option value="" >Género...</option>
-                        <option value="Masculino">Masculino</option>
-                        <option value="Femenino">Femenino</option>
-                        <option value="Prefiero no decir">Prefiero no decir</option>
+                }               
 
-                    </select>                  
-                </div>
-
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="firstName" className="label">
-                        Fecha de nacimiento:
-                    </label>
-                    <input
-                        placeholder="Fecha de nacimiento..."
-                        type='date'
-                        className="input"
-                        {...register('birthday')}
-                    />
-                    {}
-                </div>
-
-                {/* TELEFONO */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="phone" className="label">
-                        Teléfono:
-                    </label>
-                    <input
-                        placeholder="Teléfono..."
-                        className="input"
-                        {...register('phone', {
-                            maxLength: 15,
-                            pattern: /^[0-9]+$/
-                        })}
-                    />
-                    {
-                        errors.phone?.type === 'maxLength' ?
-                        <span className="text-red-500 text-xs">
-                            El teléfono debe tener máximo 15 caracteres.
-                        </span>
-                        :
-                        null                        
-                    }
-                    {
-                        errors.phone?.type === 'pattern' ?
-                        <span className="text-red-500 text-xs">
-                            Ingresa solo datos numéricos.
-                        </span>
-                        :
-                        null                        
-                    }
-                </div>
-                {/* DIRECCION */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="address" className="label">
-                        Dirección:
-                    </label>
-                    <input
-                        placeholder="Dirección..."
-                        className="input"
-                        {...register('address', {
-                            maxLength: 100,
-                        })}
-                    />
-                    {
-                        errors.address?.type === 'maxLength' ?
-                        <span className="text-red-500 text-xs">
-                            Danos una descripción más corta de tu dirección.
-                        </span>
-                        :
-                        null                        
-                    }
-                </div>
-
-                {/* CIUDAD */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="city" className="label">
-                        Ciudad:
-                    </label>
-                    <input
-                        placeholder="Ciudad..."
-                        className="input"
-                        {...register('city', {
-                            maxLength: 20,
-                        })}
-                    />
-                    {
-                        errors.city?.type === 'maxLength' ?
-                        <span className="text-red-500 text-xs">
-                            El nombre de la ciudad no puede ser mayor a 20 caracteres.
-                        </span>
-                        :
-                        null                        
-                    }
-                </div>
-
-                {/* PROVINCE */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="province" className="label">
-                        Provincia:
-                    </label>
-                    <input
-                        placeholder="Provincia..."
-                        className="input"
-                        {...register('province', {
-                            maxLength: 20,
-                        })}
-                    />
-                    {
-                        errors.province?.type === 'maxLength' ?
-                        <span className="text-red-500 text-xs">
-                            El nombre de la provincia no puede ser mayor a 20 caracteres.
-                        </span>
-                        :
-                        null                        
-                    }
-                </div>
-
-                {/* CODIGO POSTAL */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label htmlFor="postCode" className="label">
-                        Código postal:
-                    </label>
-                    <input
-                        placeholder="Código postal..."
-                        className="input"
-                        {...register('postCode', {
-                            maxLength: 10,
-                        })}
-                    />
-                    {
-                        errors.postCode?.type === 'maxLength' ?
-                        <span className="text-red-500 text-xs">
-                            El código postal no puede ser mayor a 10 caracteres.
-                        </span>
-                        :
-                        null                        
-                    }
-                </div>
-
-                {/* FOTO */}
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <label className="label">
-                        Foto:
-                    </label>
-                    <input
-                        onChange={(e) => handleChange(e)}
-                        className="input"
-                        id="photo"
-                        type="file"
-                        multiple
-                        accept="image/*"
-                    />
-                </div> 
-                
-                <button type='submit' className="text-center bg-pwgreen-500 py-3 my-2 rounded-md shadow-xl text-pwgreen-900 font-bold uppercase font-Rubik">
-                    ACTUALIZAR DATOS
-                </button>
-            </form>
+            </div>
         </div>
     )
 }
