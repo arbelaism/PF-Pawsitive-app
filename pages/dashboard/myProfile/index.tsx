819aa57cf31475a0d6ff5359b81fbfdb0f4407dc
nestordqa <ndqa96@gmail.com>
2022-12-15 07:46:42 +0100

    fixing errors

--- pages/dashboard/myProfile/index.tsx
+++ pages/dashboard/myProfile/index.tsx
@@ -1,9 +1,9 @@
 import { DashboardLayout } from 'components/dashboard'
-import Profile from 'components/dashboard/dashboardUser/profile'
+import Profile from 'components/dashboard/dashboardUser/Profile'
 
 const MyProfile = () => {
     return (
-        <DashboardLayout title="Usuarios">
+        <DashboardLayout title="Mi Perfil">
             <Profile />
         </DashboardLayout>
     )
