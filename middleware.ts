9eb053fbcb3ba0ce2a082b935fc93e930e4a3532
arbelais <arbelaism@outlook.com>
2022-12-21 20:20:06 -0300

    .

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ import 'regenerator-runtime'
 const auth0 = initAuth0({
     secret: process.env.AUTH0_SECRET,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-    baseURL: process.env.AUTH0_BASE_URL,
+    baseURL: 'https://pf-pawsitive-app-git-develop-arbelais.vercel.app/',
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET
 })
