b321d88068e35fb6861b67a87b8844463c069bbb
arbelais <arbelaism@outlook.com>
2022-12-21 21:03:28 -0300

    .

--- utils/auth0.ts
+++ utils/auth0.ts
@@ -1,9 +1,9 @@
-import { initAuth0 } from '@auth0/nextjs-auth0'
+import { initAuth0 } from "@auth0/nextjs-auth0";
 
 export default initAuth0({
     secret: process.env.AUTH0_SECRET,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-    baseURL: 'https://pf-pawsitive-app-git-develop-arbelais.vercel.app/',
+    baseURL: process.env.AUTH0_BASE_URL,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET
 })
