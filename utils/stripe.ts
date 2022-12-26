f269ee019f0fb69899eafa4de90ba74f9dd1a0cd
arbelais <arbelaism@outlook.com>
2022-12-26 20:24:35 -0300

    fix stripe key error

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -2,9 +2,13 @@ import { Stripe, loadStripe } from '@stripe/stripe-js'
 
 let stripePromise: Promise<Stripe | null>
 
+const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY
+
 const getStripe = () => {
+    if (!STRIPE_PUBLIC_KEY) return
+
     if (!stripePromise) {
-        stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!)
+        stripePromise = loadStripe(STRIPE_PUBLIC_KEY)
     }
 
     return stripePromise
